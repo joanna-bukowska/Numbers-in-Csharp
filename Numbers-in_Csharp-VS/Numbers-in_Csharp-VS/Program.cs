3a1a0683ef56bcc2ce44193693398b6461a7662b
Joanna Bukowska <joabuk2@gmail.com>
2020-06-26 22:17:31 +0200

    Basic mathematical operations

--- Numbers-in_Csharp-VS/Numbers-in_Csharp-VS/Program.cs
+++ Numbers-in_Csharp-VS/Numbers-in_Csharp-VS/Program.cs
@@ -10,12 +10,21 @@ namespace Numbers_in_Csharp_VS
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            int a = 8;
+            int b = 5;
+     
+            int c = a + b;
+            Console.WriteLine($"Addiction: {c}");
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            c = a - b;
+            Console.WriteLine($"Subtraction: {c}");
+
+            c = a * b;
+            Console.WriteLine($"Multiplication: {c}");
+
+            c = a / b;
+            Console.WriteLine($"Division: {c}");
+            Console.ReadKey();
         }
     }
 }
